5259116483c3d63530df320a4ad46bbe072c4aec
Lrodg00 <77740668+Lrodg00@users.noreply.github.com>
2021-03-23 16:09:34 -0400

    added readline

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 // 1 liter = .2641720524
 // f(mpg) = mpg * 1.609344 kpm * 0.2641720524 gpl
 
+
+let readline = require('mpg2kpl')
+
 let kpm = 1.609344
 let gpl = 0.2641720524
 
@@ -9,6 +12,12 @@ function mpg2kpl(mpg) {
     return mpg * kpm * gpl
 }
 
-console.log(mpg2kpl(52))
+let rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
 
-
+rl.question('Enter MPG value: ', (mpg) => {
+    console.log(mpg2kpl(mpg))
+    rl.close()
+})
